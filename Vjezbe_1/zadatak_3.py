804733635957a3301a21099f4268528761b321b4
jjercic <jjercic@pmfst.hr>
2023-03-06 22:25:24 +0100

    Create zadatak_3.py

---
+++ Vjezbe_1/zadatak_3.py
@@ -0,0 +1,27 @@
+##Napišite program koji će korisnika tražiti da upiše (x,y) koordinate za dvije točke. 
+##Ako korisnik pogriješi prilikom unosa koordinate opomenite ga da ponovi upis. 
+##Nakon što je korisnik uspješno upisao dvije koordinate ispišite na ekran jednadžbu pravca koji prolazi kroz te dvije točke.
+print("Unesite (x,y) koordinate za dvije točke: ")
+
+try:
+    print("Točka A: ")
+    xa = int(input("x = "))
+    ya = int(input("y = "))
+
+    print("Točka B: ")
+    xb = int(input("x = "))
+    yb = int(input("y = "))
+
+except:
+    print("Niste unijeli broj. Pokušajte ponovno!")
+    print("Točka A: ")
+    xa = int(input("x = "))
+    ya = int(input("y = "))
+
+    print("Točka B: ")
+    xb = int(input("x = "))
+    yb = int(input("y = "))
+
+k = (yb - ya) / (xb - xa)
+l = ya - xa * k
+print("y = {}x + {}".format(k, l))
