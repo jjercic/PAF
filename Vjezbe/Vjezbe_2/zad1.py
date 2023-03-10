fe0fc0b1dfb6dfb2effa235a7a14c00c81293e53
jjercic <jjercic@pmfst.hr>
2023-03-10 15:04:23 +0100

    Update zad1.py

--- Vjezbe/Vjezbe_2/zad1.py
+++ Vjezbe/Vjezbe_2/zad1.py
@@ -15,7 +15,40 @@ v0 = 0 ##početna brzina je nula?
 v = v0 + a*dt
 s = v0*dt + 0.5*a*dt*dt
 
-plt, ax = plt.subplots(figsize=(12, 6))
+#a-t
+plt.title("a - t")
+plt.xlabel("t / s")
+plt.ylabel("a / m/s^2")
 
 plt.axhline(y = a)
+plt.xlim(0, 10)
+plt.ylim(0, 1.5 * a)
+
+plt.show()
+
+##v-t
+plt.title("v - t")
+plt.xlabel("t / s")
+plt.ylabel("v / m/s")
+           
+ts = np.linspace(0, dt)
+vs = a * ts
+
+plt.xlim(0, dt)
+plt.ylim(0, dt * a)
+plt.plot(ts, vs)
+
+plt.show()
+
+#s-t
+plt.title("s - t")
+plt.xlabel("t / s")
+plt.ylabel("s / m")
+           
+ss = 0.5 * a * ts *ts
+
+plt.xlim(0, dt)
+plt.ylim(0, 0.5 * a * dt *dt)
+plt.plot(ts, ss)
+
 plt.show()
