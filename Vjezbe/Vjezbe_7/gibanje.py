1ef53422c25753504a9bb08027fb3f4cd5e6efb7
jjercic <jjercic@pmfst.hr>
2023-05-11 01:53:40 +0200

    commit

--- Vjezbe/Vjezbe_7/gibanje.py
+++ Vjezbe/Vjezbe_7/gibanje.py
@@ -1,14 +1,14 @@
 import projectile as proj
 import matplotlib.pyplot as plt
 
-for dt in [0.1, 0.01, 0.001]:
+for dt in [0.1, 0.05, 0.01, 0.001]:
     pr = proj.Projectile(0.1, 10, 60, dt)
     x, y = pr.gibanje()
     plt.plot(x, y, label = "$dt = {}$ (Euler)".format(dt))
 
 pr4 = proj.Projectile(0.1, 10, 60, 0.01)
 x4, y4 = pr4.gibanje_rk4()
-plt.plot(x4, y4, label = "$dt = {}$ (Runge-Kutta [4])".format(pr4.dt))
+plt.plot(x4, y4, label = "$dt = {}$ (Runge-Kutta 4)".format(pr4.dt))
 
 plt.title("$x - y$")
 plt.xlabel("$x  [m]$")
