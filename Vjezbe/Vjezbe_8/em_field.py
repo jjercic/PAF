ec7c97622f10a155439ea3558fb1d9e35d6d83a5
jjercic <jjercic@pmfst.hr>
2023-05-21 19:50:19 +0200

    commit

--- Vjezbe/Vjezbe_8/em_field.py
+++ Vjezbe/Vjezbe_8/em_field.py
@@ -4,21 +4,24 @@ import particle_em as prt
 
 E = np.array((0, 0, 0))
 B = np.array((0, 0, 1))
-v = np.array((0.1, 0.1, 0.1))
 q = m = 1
 t = 20
-
-ele = prt.Particle_EB(-q, m, E, B, v)
+#gibanje elektrona - Eulrova metoda
+ele = prt.Particle_EB(-q, m, E, B, np.array((0.1, 0.1, 0.1)))
 xe, ye, ze = ele.gibanje(t)
 
-pos = prt.Particle_EB(q, m, E, B, v)
-xp, yp, zp = pos.gibanje(t)
+#gibanje pozitrona - Runge-kutta metoda 4. reda
+pos = prt.Particle_EB(+q, m, E, B, np.array((0.1, 0.1, 0.1)))
+xp, yp, zp = pos.gibanje(t, 'rk4')
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax = plt.axes(projection = '3d')
 
-ax.plot3D(xe, ye, ze)
-ax.plot3D(xp, yp, zp)
+ax.plot3D(xe, ye, ze, label = "elektorn [Euler]")
+ax.plot3D(xp, yp, zp, label = "pozitron [Runge-Kutta]")
 
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
 plt.show()
 
--- Vjezbe/Vjezbe_8/particle_em.py
+++ Vjezbe/Vjezbe_8/particle_em.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Particle_EB:
-    def __init__(self, q, m, E, B, v, dt = 0.001):
+    def __init__(self, q, m, E, B, v, dt = 0.01):
         self.q = q
         self.m = m
         self.E = E
@@ -13,19 +13,44 @@ class Particle_EB:
         self.x = [0]        
         self.y = [0]        
         self.z = [0]
-        self.a = 0
+        self.a = [[0, 0, 0]]
         self.t = [0]
 
     def __move(self):
-        self.t.append(self.t[-1] + self.dt)
+        self.a = self.q*(self.E + np.cross(self.v, self.B))/self.m
+        self.v += self.a*self.dt
         self.x.append(self.x[-1] + self.v[0]*self.dt)
         self.y.append(self.y[-1] + self.v[1]*self.dt)
         self.z.append(self.z[-1] + self.v[2]*self.dt)
-        self.v += self.a*self.dt
-        self.a = self.q*(self.E + np.cross(self.v, self.B))/self.m
+        self.t.append(self.t[-1] + self.dt)
 
-    def gibanje(self, t0):
+    def gibanje(self, t0, mode = 'eul'):
         while self.t[-1] <= t0:
-            self.__move()
+            if mode == 'eul':
+                self.__move()
+            elif mode == 'rk4':
+                self.__move_rk4()
         return self.x, self.y, self.z
 
+    def a_v(self, k_i = 0):
+        return self.q*(self.E + np.cross(self.v + k_i, self.B))/self.m
+    
+    def __move_rk4(self):
+        self.t.append(self.t[-1] + self.dt)
+
+        k1_v = self.a_v() * self.dt
+        k1 = self.v * self.dt
+
+        k2_v = self.a_v(k1_v/2) * self.dt
+        k2 = (self.v + k1_v/2) * self.dt  
+
+        k3_v = self.a_v(k2_v/2)* self.dt
+        k3 = (self.v + k2_v/2) * self.dt
+
+        k4_v = self.a_v(k3_v/2)* self.dt
+        k4 = (self.v + k3_v/2) * self.dt
+
+        self.v += 1/6 * (k1_v + 2 * k2_v + 2 * k3_v + k4_v)
+        self.x.append(self.x[-1] + 1/6 * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]))
+        self.y.append(self.y[-1] + 1/6 * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]))
+        self.z.append(self.z[-1] + 1/6 * (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2]))
