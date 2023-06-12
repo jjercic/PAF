978e38f0e8020b3607c543d9cf5241948872aa1e
jjercic <jjercic@pmfst.hr>
2023-06-12 07:43:55 +0200

    commit sem

--- Seminar/DoublePendulum.py
+++ Seminar/DoublePendulum.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation
 g = 9.81
 
 class DoublePendulum:
-    def __init__(self, l1 = 1.5, l2 = 1, m1 = 1.5, m2 = 1, theta1 = 90, theta2 = 90, omega1 = 0, omega2 = 0, T = 5, dt = 0.01):
+    def __init__(self, l1 = 2, l2 = 1, m1 = 1, m2 = 1, theta1 = 120, theta2 = 90, omega1 = 0, omega2 = 0, T = 5, dt = 0.0075):
         #duljine njihala
         self.l1 = l1
         self.l2 = l2
@@ -27,25 +27,31 @@ class DoublePendulum:
 
         self.pend1 = None
         self.pend2 = None
-        self.timer_text = None
+        self.timer = None
         self.animation = None
 
     def diff_solve(self, theta1, theta2, omega1, omega2):
-        theta1_ = omega1
-        theta2_ = omega2
+        theta1_d = omega1
+        theta2_d = omega2
 
         denom = 2 * self.m1 + self.m2 - self.m2 * np.cos(2*(theta1 - theta2))
-        omega1_ = - g * (2 * self.m1 + self.m1) * np.sin(theta1) - self.m2 * g * np.sin(theta1 - 2 * theta2) - 2 * np.sin(theta1 - theta2) * self.m2 * (self.l2 * omega2**2 + self.l1 * np.cos(theta1 -theta2) * omega1) / (self.l1 * denom)
-        omega2_ = 2 * np.sin(theta1 - theta2) * (self.l1 * (self.m1 + self.m2) * omega1**2 + g * (self.m1 + self.m2) * np.cos(theta1) + self.l2 * self.m2 * np.cos(theta1 - theta2) * omega2**2) / (self.l2 * denom)
-        return theta1_, theta2_, omega1_, omega2_
+        omega1_d = - g * (2 * self.m1 + self.m2) * np.sin(theta1) 
+        - self.m2 * g * np.sin(theta1 - 2 * theta2) 
+        - 2 * np.sin(theta1 - theta2) * self.m2 * (self.l2 * omega2**2 + self.l1 * np.cos(theta1 -theta2) * omega1**2) / (self.l1 * denom)
+        
+        omega2_d = 2 * np.sin(theta1 - theta2) * (self.l1 * (self.m1 + self.m2) * omega1**2 
+        + g * (self.m1 + self.m2) * np.cos(theta1) 
+        + self.l2 * self.m2 * np.cos(theta1 - theta2) * omega2**2) / (self.l2 * denom)
+        
+        return theta1_d, theta2_d, omega1_d, omega2_d
     
     def __move(self, frame):
-        theta1_, theta2_, omega1_, omega2_ = self.diff_solve(self.theta1[-1], self.theta2[-1], self.omega1[-1], self.omega2[-1])
+        theta1_d, theta2_d, omega1_d, omega2_d = self.diff_solve(self.theta1[-1], self.theta2[-1], self.omega1[-1], self.omega2[-1])
 
-        self.theta1.append(self.theta1[-1] + theta1_ * self.dt)        
-        self.theta2.append(self.theta2[-1] + theta2_ * self.dt)
-        self.omega1.append(self.omega1[-1] + omega1_ * self.dt)
-        self.omega2.append(self.omega2[-1] + omega2_ * self.dt)
+        self.theta1.append(self.theta1[-1] + theta1_d * self.dt)        
+        self.theta2.append(self.theta2[-1] + theta2_d * self.dt)
+        self.omega1.append(self.omega1[-1] + omega1_d * self.dt)
+        self.omega2.append(self.omega2[-1] + omega2_d * self.dt)
 
         x1 = self.l1 * np.sin(self.theta1[-1])
         y1 = - self.l1 * np.cos(self.theta1[-1])        
@@ -62,26 +68,29 @@ class DoublePendulum:
         self.pend2.set_data([x1, x2], [y1, y2])
 
         self.t += self.dt
-        self.timer_text.set_text("Time: {:.2f} s".format(self.t))
+        self.timer.set_text("Time: {:.2f} s".format(self.t))
 
         if self.t >= self.T:
             self.animation.event_source.stop()
             plt.close()
 
-        return self.pend1, self.pend2, self.timer_text
+        return self.pend1, self.pend2, self.timer
     
     def animate(self):
         fig, ax = plt.subplots()
         l = self.l1 + self.l2
+        l *= 1.15
         ax.set_xlim(-l, l)
         ax.set_ylim(-l, l)
+        ax.set_aspect('equal')
+
         plt.title("Double Pendulum")
         plt.xlabel("x [m]")
         plt.ylabel("y [m]")
 
-        self.pend1, = ax.plot([], [], 'bo-')
-        self.pend2, = ax.plot([], [], 'go-')
-        self.timer_text = ax.text(
+        self.pend1, = ax.plot([], [], 'mo-')
+        self.pend2, = ax.plot([], [], 'co-')
+        self.timer = ax.text(
             0.1,
             0.9,
             "",
@@ -93,12 +102,16 @@ class DoublePendulum:
         self.animation = FuncAnimation(
             fig,
             self.__move,
-            frames = int(self.T / self.dt),
-            interval = self.dt,
-            blit = True
+            frames = int(1/self.dt),
+            interval = self.dt * 1000,
         )
-
+        self.animation.save('animation.gif')
         plt.show()
 
 pend = DoublePendulum()
 pend.animate()
+
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+plt.plot(pend.x2, pend.y2)
+plt.show()
